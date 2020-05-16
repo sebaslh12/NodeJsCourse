21fac15fab18aabc0b2be5ccff67d2b8a6f63f4c
sebaslh12 <sebaslh12@gmail.com>
2020-05-16 14:00:34 -0500

    Importing npm modules.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,10 @@
 // We can require our own files, this makes the file to run alognside the current file.
 // If we just require a file that means it is not exporting anything, if the file exports something then that something has to be store in a variable
+const validator = require('validator'); // Importing npm modules is like importing core modules, we pass the name of the package
 const { name, add } = require('./utils');
 const getNotes = require('./notes');
 const msg = getNotes();
 
 console.log(add(4, -2), name);
 console.log(msg);
+console.log(validator.isEmail('a@.com'));
