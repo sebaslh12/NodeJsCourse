8dcaa28a15457ef90d917873c98c4713dd09a850
sebaslh12 <sebaslh12@gmail.com>
2020-05-16 14:08:58 -0500

    Section 1: Challenge.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,6 @@
 // We can require our own files, this makes the file to run alognside the current file.
 // If we just require a file that means it is not exporting anything, if the file exports something then that something has to be store in a variable
+require('./chalkChallenge');
 const validator = require('validator'); // Importing npm modules is like importing core modules, we pass the name of the package
 const { name, add } = require('./utils');
 const getNotes = require('./notes');
---
+++ notes-app/chalkChallenge.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+
+const success = chalk.bold.inverse.green('Success!!')
+
+console.log(success)
