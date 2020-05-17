1a6fb3e7793795fdb62ce104918969fecae0335f
sebaslh12 <sebaslh12@gmail.com>
2020-05-17 10:03:48 -0500

    Argument parsing with Yargs I.

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,11 +2,31 @@
 // If we just require a file that means it is not exporting anything, if the file exports something then that something has to be store in a variable
 // Importing npm modules is like importing core modules, we pass the name of the package
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes');
 
-const command = process.argv[2]
+yargs.command({
+	command: 'add',
+	describe: 'Add a new note',
+	handler: () => console.log('Adding a new note')
+});
 
-console.log(process.argv);
+yargs.command({
+	command: 'remove',
+	describe: 'Removes a note',
+	handler: () => console.log('Removing a note')
+});
 
-if (command === 'add') console.log('Adding note!');
-else if (command === 'remove') console.log('Removing note!');
+yargs.command({
+	command: 'list',
+	describe: 'List the notes',
+	handler: () => console.log('Listing notes')
+});
+
+yargs.command({
+	command: 'read',
+	describe: 'Read note',
+	handler: () => console.log('Reading note')
+});
+
+console.log(yargs.argv)
