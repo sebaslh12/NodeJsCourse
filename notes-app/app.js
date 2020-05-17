30811119febba931926e30f8fd242a61bcfba855
sebaslh12 <sebaslh12@gmail.com>
2020-05-17 09:47:41 -0500

    Getting input from users

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,11 +1,12 @@
 // We can require our own files, this makes the file to run alognside the current file.
 // If we just require a file that means it is not exporting anything, if the file exports something then that something has to be store in a variable
-require('./chalkChallenge');
-const validator = require('validator'); // Importing npm modules is like importing core modules, we pass the name of the package
-const { name, add } = require('./utils');
+// Importing npm modules is like importing core modules, we pass the name of the package
+const chalk = require('chalk');
 const getNotes = require('./notes');
-const msg = getNotes();
 
-console.log(add(4, -2), name);
-console.log(msg);
-console.log(validator.isEmail('a@.com'));
+const command = process.argv[2]
+
+console.log(process.argv);
+
+if (command === 'add') console.log('Adding note!');
+else if (command === 'remove') console.log('Removing note!');
