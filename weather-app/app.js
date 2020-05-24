47c5b599cd47de0aec7c4a4f66539e2d23dea0c0
sebaslh12 <sebaslh12@gmail.com>
2020-05-24 15:06:05 -0500

    Handling errors.

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,9 +3,15 @@ const request = require('request');
 const url = 'http://api.weatherstack.com/current?access_key=77585df14f7452b66fe0e255116b2bf7&query=37.8639,-122.4233';
 
 
-request({url, json: true}, (error, response) => {
-	const data = response.body.current;
-	const temp = data.temperature;
-	const feelslike = data.feelslike;
-	console.log(`It is currently ${temp} degrees out. It feels like ${feelslike} degrees out.`);
+request({ url, json: true }, (error, response) => {
+	if (error) {
+		console.log('Unable to connect to weather service');
+	} else if (response.body.error) {
+		console.log('Unable to find location');
+	} else {
+		const data = response.body.current;
+		const temp = data.temperature;
+		const feelslike = data.feelslike;
+		console.log(`It is currently ${temp} degrees out. It feels like ${feelslike} degrees out.`);
+	}
 });
