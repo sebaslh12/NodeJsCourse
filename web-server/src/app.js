e3669292d4ebdfcf8ee8f5fbd95862aeef90d961
sebaslh12 <sebaslh12@gmail.com>
2020-05-30 17:02:39 -0500

    Serving up static assets.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,12 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+// The node main wrapper function provides __dirname and __filename values
+const publicDirectory = path.join(__dirname, '../public');
 
-app.get('', (req, res) => {
-	res.send('<h1>Weather</h1>');
-});
-
-app.get('/help', (req, res) => {
-	res.send({
-		name: 'Sebastian',
-		age: 25
-	});
-});
-
-app.get('/about', (req, res) => {
-	res.send('<h1>About</h1>');
-});
+// Express static replaces the root route
+app.use(express.static(publicDirectory));
 
 app.get('/weather', (req, res) => {
 	res.send({
@@ -24,6 +15,6 @@ app.get('/weather', (req, res) => {
 	});
 });
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
 	console.log('Server is up on port 3000.');
 });
