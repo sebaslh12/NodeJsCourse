e6e885a3a3f5a4a8bc66ebf88eaaffac0cc32237
sebaslh12 <sebaslh12@gmail.com>
2020-05-30 10:35:16 -0500

    Callback Abstraction.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,17 +1,6 @@
-const request = require('request');
+const forecast = require('./utils/forecast.js');
 
-const url = 'http://api.weatherstack.com/current?access_key=77585df14f7452b66fe0e255116b2bf7&query=37.8639,-122.4233';
-
-
-request({ url, json: true }, (error, response) => {
-	if (error) {
-		console.log('Unable to connect to weather service');
-	} else if (response.body.error) {
-		console.log('Unable to find location');
-	} else {
-		const data = response.body.current;
-		const temp = data.temperature;
-		const feelslike = data.feelslike;
-		console.log(`It is currently ${temp} degrees out. It feels like ${feelslike} degrees out.`);
-	}
+forecast('37.8639','-122.4233', (error, data)=>{
+	console.log(error);
+	console.log(data);
 });
