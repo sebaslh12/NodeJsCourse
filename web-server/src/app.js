09da903e813edd2e0171e7a3eee6ac651b6f0e4e
sebaslh12 <sebaslh12@gmail.com>
2020-05-30 18:02:43 -0500

    Dynamic Pages with Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,9 +4,31 @@ const express = require('express');
 const app = express();
 // The node main wrapper function provides __dirname and __filename values
 const publicDirectory = path.join(__dirname, '../public');
-
-// Express static replaces the root route
+const viewsDirectory = path.join(__dirname, '../views');
+app.set('views', viewsDirectory);
+app.set('view engine', 'hbs'); // Express template engine
+// Express static replaces the root route (if an index.html file exists)
 app.use(express.static(publicDirectory));
+
+app.get('', (req, res) => {
+	res.render('index', {
+		title: 'Weather',
+		name: 'Sebastian'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		title: 'About',
+		name: 'Sebastian'
+	});
+});
+
+app.get('/help', (req, res) => {
+	res.render('help', {
+		help: 'So you come to me?'
+	});
+});
 
 app.get('/weather', (req, res) => {
 	res.send({
