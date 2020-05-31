89c1d80fbc5a2235d40477ac298b48b22226c610
sebaslh12 <sebaslh12@gmail.com>
2020-05-31 12:25:38 -0500

    Advance templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,17 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 // The node main wrapper function provides __dirname and __filename values
 const publicDirectory = path.join(__dirname, '../public');
-const viewsDirectory = path.join(__dirname, '../views');
+const viewsDirectory = path.join(__dirname, '../templates/views');
+const partialsDirectory = path.join(__dirname, '../templates/partials');
+
 app.set('views', viewsDirectory);
 app.set('view engine', 'hbs'); // Express template engine
+hbs.registerPartials(partialsDirectory);
+
 // Express static replaces the root route (if an index.html file exists)
 app.use(express.static(publicDirectory));
 
@@ -26,14 +31,17 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
 	res.render('help', {
-		help: 'So you come to me?'
+		help: 'So you come to me?',
+		title: 'Help',
+		name: 'Sebastian'
 	});
 });
 
 app.get('/weather', (req, res) => {
 	res.send({
 		forecast: 'It is raining',
-		location: 'Colombia'
+		location: 'Colombia',
+		name: 'Sebastian'
 	});
 });
 
