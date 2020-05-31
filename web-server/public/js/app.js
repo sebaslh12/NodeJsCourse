34bb411326588f134e8b5f2a521cfcc1f64918bc
sebaslh12 <sebaslh12@gmail.com>
2020-05-31 16:11:13 -0500

    Wiriing up the Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,16 @@
 const weatherForm = document.querySelector('form');
+const loading = document.getElementById('loading');
+const forecast = document.getElementById('forecast');
 
 weatherForm.addEventListener('submit', (event) => {
 	event.preventDefault();
 	const location = event.target.location.value;
+	loading.textContent = 'Loading...';
+	forecast.textContent = '';
 	fetch(`http://localhost:3000/weather?address=${location}`).then(async (response) => {
+		loading.textContent = '';
 		const data = await response.json();
-		if (data.error) console.log(data.error);
-		else console.log(data.location, data.forecast);
+		if (data.error) forecast.textContent = data.error;
+		else forecast.textContent = `${data.location}. ${data.forecast}`;
 	});
 })
