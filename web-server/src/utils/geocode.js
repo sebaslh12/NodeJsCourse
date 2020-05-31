c6124eabec5b396d6da0d068fafa3f2387103143
sebaslh12 <sebaslh12@gmail.com>
2020-05-31 16:13:21 -0500

    Fixing geocode query.

--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -4,9 +4,10 @@ const geocode = (address, callback) => {
 	const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=put_your_token_here`;
 
 	request({ url, json: true }, (error, { body }) => {
+		console.log(error, body);
 		if (error) {
 			callback('Unable to connect to location services!', undefined);
-		} else if (!body.features || body.message) {
+		} else if (!body.features || !body.features.length  || body.message) {
 			callback('Unable to find location. Try another search.', undefined);
 		} else {
 			callback(undefined, {
