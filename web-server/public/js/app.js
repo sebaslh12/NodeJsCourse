c7abb56bc57a3539595cd1b0fd17af9c39984a2b
sebaslh12 <sebaslh12@gmail.com>
2020-05-31 16:00:57 -0500

    Fetch Requests.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,6 @@
-console.log('App running')
+
+fetch('http://localhost:3000/weather?address=Boston').then(async (response) => {
+	const data = await response.json();
+	if (data.error) console.log(data.error);
+	else console.log(data.location, data.forecast);
+});
