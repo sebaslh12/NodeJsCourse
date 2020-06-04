3c7e10dc31df436c46d04d421e1d3a7d33b9f39e
sebaslh12 <sebaslh12@gmail.com>
2020-06-04 12:04:32 -0500

    Inserting Documents

--- task-app/mongodb.js
+++ task-app/mongodb.js
@@ -6,8 +6,36 @@ const database = 'task-manager';
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
 	if (error) return console.log('Unable to connect');
 	const db = client.db(database);
-	db.collection('users').insertOne({
-		name:'Sebastian',
+	/* db.collection('users').insertOne({
+		name: 'Sebastian',
 		age: '25'
-	});
+	}, (error, result) => {
+		if (error) return console.log('Unable to insert user');
+		console.log(result.ops);
+	}); */
+
+	/* db.collection('users').insertMany([{
+		name: 'Jen',
+		age: 28
+	}, {
+		name: 'Gunther',
+		age: 22
+	}], (error, result) => {
+		if (error) return console.log('Unable to insert user');
+		console.log(result.ops);
+	}) */
+
+	db.collection('tasks').insertMany([{
+		description: 'Finish the course',
+		completed: false
+	}, {
+		description: 'Finish the book',
+		completed: false
+	}, {
+		description: 'Complete the challenge',
+		completed: true
+	}], (error, result) => {
+		if (error) return console.log('Unable to insert user');
+		console.log(result.ops);
+	})
 });
