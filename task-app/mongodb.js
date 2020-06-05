8ca871acb7944cfd29ee657199714ca845702f57
sebaslh12 <sebaslh12@gmail.com>
2020-06-05 12:05:58 -0500

    Querying Documents.

--- task-app/mongodb.js
+++ task-app/mongodb.js
@@ -1,41 +1,17 @@
-const mongodb = require("mongodb");
+const { MongoClient, ObjectID } = require("mongodb");
+
 const connectionURL = 'mongodb://127.0.0.1:27017';
-const MongoClient = mongodb.MongoClient
 const database = 'task-manager';
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
 	if (error) return console.log('Unable to connect');
 	const db = client.db(database);
-	/* db.collection('users').insertOne({
-		name: 'Sebastian',
-		age: '25'
-	}, (error, result) => {
-		if (error) return console.log('Unable to insert user');
-		console.log(result.ops);
-	}); */
+	db.collection('users').findOne({ name: 'Sebastian' }, (error, user) => {
+		if(error) return console.log('Unable to fetch');
+		console.log(user)
+	});
 
-	/* db.collection('users').insertMany([{
-		name: 'Jen',
-		age: 28
-	}, {
-		name: 'Gunther',
-		age: 22
-	}], (error, result) => {
-		if (error) return console.log('Unable to insert user');
-		console.log(result.ops);
-	}) */
-
-	db.collection('tasks').insertMany([{
-		description: 'Finish the course',
-		completed: false
-	}, {
-		description: 'Finish the book',
-		completed: false
-	}, {
-		description: 'Complete the challenge',
-		completed: true
-	}], (error, result) => {
-		if (error) return console.log('Unable to insert user');
-		console.log(result.ops);
+	db.collection('users').find({ name: 'Sebastian' }).toArray((error, users) => {
+		console.log(users);
 	})
 });
