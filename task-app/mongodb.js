242b4ebebb7662f153c9bb886aa72a4788e19dc0
sebaslh12 <sebaslh12@gmail.com>
2020-06-05 12:13:21 -0500

    Updating documents.

--- task-app/mongodb.js
+++ task-app/mongodb.js
@@ -6,12 +6,18 @@ const database = 'task-manager';
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
 	if (error) return console.log('Unable to connect');
 	const db = client.db(database);
-	db.collection('users').findOne({ name: 'Sebastian' }, (error, user) => {
-		if(error) return console.log('Unable to fetch');
+	/* db.collection('users').findOne({ name: 'Sebastian' }, (error, user) => {
+		if (error) return console.log('Unable to fetch');
 		console.log(user)
 	});
 
 	db.collection('users').find({ name: 'Sebastian' }).toArray((error, users) => {
 		console.log(users);
+	}); */
+
+	db.collection('users').updateOne({ _id: ObjectID("5ed927683105522eaf816ecc") }, { $set: { name: 'Mike' } }).then((result) => {
+		console.log(result);
+	}).catch((error) => {
+		console.log(error);
 	})
 });
