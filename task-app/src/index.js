3af3121ca1b2307c5a09b55b1be8c8a5ba4ce68b
sebaslh12 <sebaslh12@gmail.com>
2020-06-07 11:07:38 -0500

    Resource Reading Endpoint II

--- task-app/src/index.js
+++ task-app/src/index.js
@@ -45,6 +45,26 @@ app.post('/tasks', (req, res) => {
 	});
 });
 
+app.get('/tasks', (req, res) => {
+	Task.find({}).then((tasks) => {
+		res.status(201).send(tasks);
+	}).catch((e) => {
+		res.status(500).send(e);
+	});
+});
+
+app.get('/tasks/:id', (req, res) => {
+	const _id = req.params.id;
+	Task.findById(_id).then((task) => {
+		if (!task) return res.status(404).send();
+		res.send(task);
+	}).catch((e) => {
+		if (e.name === 'CastError') return res.status(400).send({ reason: 'Argument passed in must be a single String of 12 bytes or a string of 24 hex characters.' });
+		res.status(500).send(e);
+	});
+});
+
+
 app.listen(port, () => {
 	console.log(`Server is up on: ${port}`);
 });
