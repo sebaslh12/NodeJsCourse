8d861360008f0b1dc9fbc8306f7a1f4833ed88f3
sebaslh12 <sebaslh12@gmail.com>
2020-06-07 10:33:17 -0500

    Resource Creating Endpoint II

--- task-app/src/db/mongoose.js
+++ task-app/src/db/mongoose.js
@@ -3,15 +3,3 @@ const mongoose = require('mongoose');
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager';
 
 mongoose.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
-
-const Task = mongoose.model('Task', {
-	description: {
-		type: String,
-		required: true,
-		trim: true
-	},
-	completed: {
-		default: false,
-		type: Boolean
-	}
-});
