ccf22c2d0e97bb09c27a68537ca268f599186262
sebaslh12 <sebaslh12@gmail.com>
2020-06-20 11:13:01 -0500

    Authenticating Task Endpoints

--- task-app/src/routers/tasks.js
+++ task-app/src/routers/tasks.js
@@ -14,19 +14,19 @@ router.post('/tasks', auth, async (req, res) => {
 	}
 });
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
 	try {
-		const tasks = await Task.find({});
-		res.status(201).send(tasks);
+		await req.user.populate('tasks').execPopulate();
+		res.status(200).send(req.user.tasks);
 	} catch (e) {
 		res.status(500).send(e);
 	}
 });
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
 	const _id = req.params.id;
 	try {
-		const task = await Task.findById(_id);
+		const task = await Task.findOne({ _id, owner: req.user._id });
 		if (!task) return res.status(404).send();
 		res.send(task);
 	} catch (e) {
@@ -35,14 +35,16 @@ router.get('/tasks/:id', async (req, res) => {
 	}
 });
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
 	const _id = req.params.id;
 	const updates = Object.keys(req.body);
 	try {
-		const task = await Task.findById(_id);
-		updates.forEach((update) => task[update] = req.body[update]);
-		await task.save()
+		const task = await Task.findOne({ _id, owner: req.user._id });
 		if (!task) return res.status(404).send();
+
+		updates.forEach((update) => task[update] = req.body[update]);
+		await task.save();
+
 		res.send(task);
 	} catch (e) {
 		if (e.name === 'CastError') return res.status(400).send({ reason: 'Argument passed in must be a single String of 12 bytes or a string of 24 hex characters.' });
@@ -50,10 +52,10 @@ router.patch('/tasks/:id', async (req, res) => {
 	}
 });
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
 	const _id = req.params.id;
 	try {
-		const task = await Task.findByIdAndDelete(_id, req.body);
+		const task = await Task.findOneAndDelete({ _id, owner: req.user._id });
 		if (!task) return res.status(404).send();
 		res.send(task);
 	} catch (e) {
